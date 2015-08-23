32f21136abcf7ad9b99086614afe5b8e63e51744
everzet <ever.zet@gmail.com>
2015-08-23 23:54:17 +0100

    Initiate GUI suite and make context use Mink

---
+++ features/bootstrap/BasketGUIContext.php
@@ -0,0 +1,22 @@
+<?php
+
+use Behat\Behat\Context\Context;
+use Behat\Behat\Context\SnippetAcceptingContext;
+use Behat\MinkExtension\Context\RawMinkContext;
+
+/**
+ * Defines application features from the specific context.
+ */
+class BasketGUIContext extends RawMinkContext implements Context, SnippetAcceptingContext
+{
+    /**
+     * Initializes context.
+     *
+     * Every scenario gets its own context instance.
+     * You can also pass arbitrary arguments to the
+     * context constructor through behat.yml.
+     */
+    public function __construct()
+    {
+    }
+}
