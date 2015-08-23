355c96a8577c4b40d88d11ac4e3613be619813c7
everzet <ever.zet@gmail.com>
2015-08-24 00:30:58 +0100

    Implement front controller

---
+++ web/index.php
@@ -0,0 +1,33 @@
+<?php
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$app = new Silex\Application();
+$app['debug'] = true;
+
+$catalogue = new Catalogue();
+
+$app->get(
+    '/catalogue',
+    function () use ($catalogue) {
+        $page = "<ul>";
+        foreach ($catalogue as $product) {
+            $page .= "<li class='product'>{$product->sku()}<a
+            href='/catalogue/{$product->sku()}/add-to-basket'>Add to basket</a></li>";
+        }
+        $page .= "</ul>";
+
+        return $page;
+    }
+);
+
+$app->get(
+    '/catalogue/{sku}/add-to-basket',
+    function ($sku) use ($catalogue) {
+        $basket = new Basket();
+        $basket->addProductFromCatalogue(Sku::fromString($sku), $catalogue);
+
+        return "Total cost of basket: £{$basket->totalCost()}";
+    }
+);
+$app->run();
